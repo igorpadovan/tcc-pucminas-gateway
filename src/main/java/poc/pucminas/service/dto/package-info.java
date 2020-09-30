750ca95a70b20a19228e5ca089bc787924382f90
Igor Padovan da Silva <igorpadovan@gmail.com>
2020-09-29 21:33:21 -0400

    Initial version of Gateway generated by JHipster-6.10.1

---
+++ src/main/java/poc/pucminas/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package poc.pucminas.service.dto;
---
+++ src/main/java/poc/pucminas/service/mapper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MapStruct mappers for mapping domain objects and Data Transfer Objects.
+ */
+package poc.pucminas.service.mapper;
